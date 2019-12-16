ad8e6b54e4c6f58fd40ff4c8ac262a4c2ca7d190
Nipuna Marcus <nipunac@wso2.com>
2019-12-16 08:24:33 +0530

    Improve index to have ballerina module titles

--- js/site-search.js
+++ js/site-search.js
@@ -1,17 +1,29 @@
 $(function () {
-    let idx = lunr(function () {
+    let contentSearch = lunr(function () {
         this.ref('page');
         this.field('content');
-        documents.forEach(function (doc) {
+        contentIndex.forEach(function (doc) {
+            this.add(doc)
+        }, this);
+    });
+
+    let titleSearch = lunr(function () {
+        this.ref('page');
+        this.field('content');
+        titleIndex.forEach(function (doc) {
             this.add(doc)
         }, this);
     });
 
     let search = function () {
-        debugger;
         $('#searchResult').empty();
         let text = $('#searchText').val();
-        let searched = idx.search(text);
+        let searched = titleSearch.search(text);
+
+        if (searched.length <= 0) {
+            searched = contentSearch.search(text);
+        }
+
         searched.forEach(function (searchtxt) {
             $('#searchResult').append('<li><h3><a href="' + searchtxt.ref + '">' + searchtxt.ref + '</a></h3><p>'
                 + 'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in'

7791bf149f4d657dcc011f086f7077e78afabb84
thimuth <thimuth@gmail.com>
2020-06-05 02:40:49 +0530

    Custom redirection functionality

---
+++ js/redirections.js
@@ -0,0 +1,5 @@
+let redirections = {
+    // "/SOURCE URL without trailing slash":"/DESTINATION URL with slash"
+    "/learn/intellij-plugin-old":"/learn/tools-ides/setting-up-intellij-idea/",
+    "/source3":"/why/"
+};

dc4eee6eebdfdda522a2acec75d15a0bbcdb0929
praneesha <praneesha@wso2.com>
2020-10-07 12:07:02 +0530

    Fix more redirection issues

--- js/redirections.js
+++ js/redirections.js
@@ -237,11 +237,5 @@ let redirections = {
     "/learn/by-example/readonly-objects.html":"/page-not-available.html",
     "/1.1/learn/by-example/readonly-objects.html":"/1.1/page-not-available.html",
     "/1.0/learn/by-example/readonly-objects.html":"/1.0/page-not-available.html",
-    "/swan-lake/learn/api-docs/ballerina/*/objects/*":"/swan-lake/learn/api-docs/ballerina/*/classes/*",
-    "/learn/by-example/testerina-function-mocks.html":"/learn/by-example/testerina-mocking-functions.html",
-    "/learn/by-example/testerina-object-mocks.html":"/learn/by-example/testerina-mocking-objects.html",
-    "/learn/by-example/testerina-before-each-test.html":"/learn/by-example/testerina-before-and-after-each.html",
-    "/swan-lake/learn/by-example/testerina-function-mocks.html":"/swan-lake/learn/by-example/testerina-mocking-functions.html",
-    "/swan-lake/learn/by-example/testerina-object-mocks.html":"/swan-lake/learn/by-example/testerina-mocking-objects.html",
-    "/swan-lake/learn/by-example/testerina-before-each-test.html":"/swan-lake/learn/by-example/testerina-before-and-after-each.html"
+    "/swan-lake/learn/api-docs/ballerina/*/objects/*":"/swan-lake/learn/api-docs/ballerina/*/classes/*"
 };

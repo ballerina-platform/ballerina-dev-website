188ea2e6f880899e3171228c6b3066d062a8e22b
Dinushantha Meeriyagalla <dinushanthameeriyagalla@dinushan-2.local>
2022-01-12 19:06:37 +0530

    Add community page corrections

--- js/ballerina-common.js
+++ js/ballerina-common.js
@@ -538,7 +538,7 @@ $(document).ready(function() {
                 if (result.body.ok) {
                     $("#slackSuccessAlert").show(); 
                     $('#slackEmail').val('');
-                    $("#slackSuccessAlert").html( "Your invitation has been sent to "+email+".");
+                    $("#slackSuccessAlert").html( "Thank you for your interest in joining the Ballerina Slack channel. Please check your inbox for an invitation to join Slack.");
                 } else if(result.body.error == "already_in_team"){
                     $("#slackSuccessAlert").show(); 
                     $("#slackSuccessAlert").html( "This email is already subscribed.");

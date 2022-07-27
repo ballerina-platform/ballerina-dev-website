c831c5ea2e71729dcc60009a4b3b950639aaf42a
Sarani Mendis <saranimendisb@gmail.com>
2022-07-27 19:04:34 +0530

    Fix broken links

--- pages/downloads/swan-lake-release-notes/[...slug].js
+++ pages/downloads/swan-lake-release-notes/[...slug].js
@@ -89,7 +89,7 @@ export default function PostPage({ frontmatter, content, id }) {
       async function fetchData() {
         getHighlighter({
           theme: "github-light",
-          langs: ['bash', 'ballerina', 'toml', 'yaml', 'sh', 'json', 'graphql', 'sql', 'java']
+          langs: ['bash', 'ballerina', 'toml', 'yaml', 'sh', 'json', 'graphql', 'sql', 'java', 'xml']
         }).then((highlighter) => {
           setCodeSnippet(highlighter.codeToHtml(code, language));
         })

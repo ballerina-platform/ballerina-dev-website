13d5ca49732718f680c249a78a8f446b5842efdb
Sarani Mendis <saranimendisb@gmail.com>
2022-08-01 10:23:41 +0530

    Fix spacing issues and add BBE redirect

--- pages/downloads/0.9.x-release-notes/[...slug].js
+++ pages/downloads/0.9.x-release-notes/[...slug].js
@@ -165,7 +165,7 @@ export default function PostPage({ frontmatter, content, id }) {
         <Col sm={3} xxl={2} className='leftNav d-none d-sm-block'>
           <LeftNav launcher='rn' id={id}
             mainDir='0.9.x-release-notes'
-            LearnToc={RNToc} />
+            Toc={RNToc} />
         </Col>
         <Col xs={12} className='d-block d-sm-none'>
           <Button className='learnMob' onClick={handleShow}>
@@ -177,11 +177,11 @@ export default function PostPage({ frontmatter, content, id }) {
             <Offcanvas.Body>
               <LeftNav launcher='rn' id={id}
                 mainDir='0.9.x-release-notes'
-                LearnToc={RNToc} />
+                Toc={RNToc} />
             </Offcanvas.Body>
           </Offcanvas>
         </Col>
-        <Col xs={12} sm={7} xxl={8} className='mdContent'>
+        <Col xs={12} sm={7} xxl={7} className='mdContent'>
           <Container>
             <div className='topRow'>
               <Col xs={11}><h1>{frontmatter.title}</h1></Col>
@@ -286,7 +286,7 @@ export default function PostPage({ frontmatter, content, id }) {
             </div> */}
           </Container>
         </Col>
-        <Col sm={2} className='pageToc d-none d-sm-block'>
+        <Col sm={2} xxl={3} className='pageToc d-none d-sm-block'>
           {
             (showToc) ?
               <>

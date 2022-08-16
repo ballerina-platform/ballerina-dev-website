e18e2458264445e4b733310ad01c601bac0658de
Ashweni Miranda <91882446+ashweni@users.noreply.github.com>
2022-08-16 11:13:39 +0530

    Update UpcomingEvents.js

--- components/common/upcoming-events/UpcomingEvents.js
+++ components/common/upcoming-events/UpcomingEvents.js
@@ -27,19 +27,19 @@ export default function UpcomingEvents() {
     <>
       <Row className={styles.eventRows}>
         <Col sm={12} md={2} className={styles.eventDateContainer}>
-          <p className={`${styles.eventDate} ${styles.eventDateNum}`}>Aug 11, 2022</p>	
-          <p className={styles.eventDate}>Thurs., 3.00 p.m. PDT</p>	
-          <p className="eventLocation">Seattle, USA</p>
+          <p className={`${styles.eventDate} ${styles.eventDateNum}`}>Aug 23, 2022</p>	
+          <p className={styles.eventDate}>Tue., 2.30 p.m. PDT</p>	
+          <p className="eventLocation">Online</p>
         </Col>
         <Col sm={12} md={7} className={styles.eventDetail} id="eventDetails">
-          <a target="_blank" href="https://vslive.com/events/redmond-2022/home.aspx" rel="noreferrer">	
-            <p className="eventName">Visual Studio LIVE!</p>	
+          <a target="_blank" href="https://www.meetup.com/grpcio/events/287643293/" rel="noreferrer">	
+            <p className="eventName">Meetup</p>	
           </a>
-          <h5>VH19 Busy Developer&apos;s Guide to Ballerina</h5>		
-          <a target="_blank" rel="noreferrer" href="https://twitter.com/tedneward">Ted Neward</a>, Co-founder of Solidify
+          <h5>Building a gRPC Application in Ballerina</h5>		
+          <a target="_blank" rel="noreferrer" href="https://twitter.com/dknkuruppu">Danesh Kuruppu</a>, Technical Lead, WSO2
         </Col>
         <Col sm={12} md={3} className={styles.eventURL}>
-          <a className={styles.eventRegistration} href="https://vslive.com/Events/Redmond-2022/Sessions/Thursday/VH19-Busy-Developers-Guide-to-Ballerina.aspx" target="_blank" rel="noreferrer">More info</a>
+          <a className={styles.eventRegistration} href="https://www.meetup.com/grpcio/events/287643293/" target="_blank" rel="noreferrer">Register</a>
         </Col>
       </Row>
 

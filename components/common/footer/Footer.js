2cb537474b3112b304afedc631307596ca60d391
Sarani Mendis <saranimendisb@gmail.com>
2022-08-29 13:24:11 +0530

    Update discord url

--- components/common/footer/Footer.js
+++ components/common/footer/Footer.js
@@ -52,7 +52,7 @@
                          </a>
                        </li>
                        <li>
-                         <a className={styles.footerLinkSM} href="https://discord.gg/3W4X2KJ6uB" target="_blank" rel="noreferrer" passHref>
+                         <a className={styles.footerLinkSM} href="https://discord.com/invite/wAJYFbMrG2" target="_blank" rel="noreferrer" passHref>
                            <Image src={`${prefix}/images/discord-grey.svg`} width={17} height={17} alt="Discord" />
                          </a>
                        </li>

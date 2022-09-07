95605fa39d2c9c543c5112d65b6b3c81765380bc
Sarani Mendis <saranimendisb@gmail.com>
2022-09-07 16:46:58 +0530

    Fix issues in the newsletter & add missing GA scripts

--- layouts/LayoutHome.js
+++ layouts/LayoutHome.js
@@ -34,10 +34,44 @@ export default function Layout({ children }) {
 
         <meta name="description" content="Ballerina is an open-source programming language for the cloud that makes it easier to use, combine and create network services."></meta>
 
-        {/* Google analytics */}
+        {/* Google Tag Manager */}
         <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js" />
         <script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-PSL2TX4" />
         <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-92163714-2" />
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag() {
+                    dataLayer.push(arguments);
+                  }
+                  gtag("js", new Date());
+                  gtag("config", "UA-92163714-2");
+                  `
+          }}
+        />
+
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+                  (function (w, d, s, l, i) {
+                    w[l] = w[l] || [];
+                    w[l].push({
+                      "gtm.start": new Date().getTime(),
+                      event: "gtm.js",
+                    });
+                    var f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s),
+                      dl = l != "dataLayer" ? "&l=" + l : "";
+                    j.async = true;
+                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+                    f.parentNode.insertBefore(j, f);
+                  })(window, document, "script", "dataLayer", "GTM-PSL2TX4");
+                  `
+          }}
+        />
 
         <script type="text/javascript" crossOrigin="true" src="https://cdn.jsdelivr.net/npm/@docsearch/js@alpha" />
 

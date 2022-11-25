d9b924b99635b26f705f6bed38f96ac25222391f
praneesha <praneesha@wso2.com>
2022-11-25 13:54:07 +0530

    Revert "[Automated] Deploy website"

--- utils/rearrangeFiles.sh
+++ utils/rearrangeFiles.sh
@@ -6,7 +6,6 @@ cp -r ./out/learn/learn-the-platform/* ./out/learn
 cp -r ./out/policies/* ./out/
 
 #Rearrange files inside the sub-folders of learn-the-platform folder
-cp -r ./out/learn/native-support/* ./out/learn
 cp -r ./out/learn/ballerina-central/* ./out/learn
 cp -r ./out/learn/ballerina-tooling/* ./out/learn
 cp -r ./out/learn/configure-observe/* ./out/learn
@@ -17,7 +16,7 @@ cp ./out/404/index.html ./out/404.html
 
 #Remove duplicated files
 rm -rf ./out/learn/get-started/ ./out/learn/featured-use-cases/ ./out/learn/learn-the-language/ ./out/learn/learn-the-platform/ ./out/policies/
-rm -rf ./out/learn/native-support/ ./out/learn/ballerina-central/ ./out/learn/ballerina-tooling/ ./out/learn/configure-observe/ ./out/learn/java-interoperability/ ./out/learn/test-document-the-code/ ./out/learn/source-code-dependencies/
+rm -rf ./out/learn/ballerina-central/ ./out/learn/ballerina-tooling/ ./out/learn/configure-observe/ ./out/learn/java-interoperability/ ./out/learn/test-document-the-code/ ./out/learn/source-code-dependencies/
 
 
 #Edit the sitemap
@@ -27,7 +26,6 @@ sed -i 's/learn-the-language\///g' ./out/sitemap.xml
 sed -i 's/learn-the-platform\///g' ./out/sitemap.xml
 sed -i 's/policies\///g' ./out/sitemap.xml
 
-sed -i 's/native-support\///g' ./out/sitemap.xml
 sed -i 's/ballerina-central\///g' ./out/sitemap.xml
 sed -i 's/ballerina-tooling\///g' ./out/sitemap.xml
 sed -i 's/configure-observe\///g' ./out/sitemap.xml

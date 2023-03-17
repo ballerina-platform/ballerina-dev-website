9ac131606d512a4ba0520148e7d10833ae99df0a
Sarani Mendis <saranimendisb@gmail.com>
2023-03-17 18:42:21 +0530

    Update content

--- pages/usecases/ai/index.js
+++ pages/usecases/ai/index.js
@@ -117,14 +117,14 @@ export default function Integrations({ samples }) {
         <meta
           property="og:image"
           itemProp="image"
-          content="https://ballerina.io/images/ballerina-for-integration-sm-banner.png"
+          content="https://ballerina.io/images/ai-sm-banner.png"
         />
 
         {/* LINKED IN */}
         <meta property="og:title" content="Ballerina" />
         <meta
           property="og:image"
-          content="https://ballerina.io/images/ballerina-for-integration-sm-banner.png"
+          content="https://ballerina.io/images/ai-sm-banner.png"
         />
         <meta
           property="og:description"
@@ -144,7 +144,7 @@ export default function Integrations({ samples }) {
         />
         <meta
           name="twitter:image"
-          content="https://ballerina.io/images/ballerina-for-integration-sm-banner.png"
+          content="https://ballerina.io/images/ai-sm-banner.png"
         />
         <meta
           property="twitter:text:description"
@@ -152,7 +152,7 @@ export default function Integrations({ samples }) {
         />
         <meta
           property="twitter:image"
-          content="https://ballerina.io/images/ballerina-for-integration-sm-banner.png"
+          content="https://ballerina.io/images/ai-sm-banner.png"
         />
       </Head>
 

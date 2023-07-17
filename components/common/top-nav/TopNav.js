91fb03948c6c3d2a0afe4e09ecbbdc2f6b904895
Sarani Mendis <saranimendisb@gmail.com>
2023-07-17 16:18:10 +0530

    Remove unused components

--- components/common/top-nav/TopNav.js
+++ components/common/top-nav/TopNav.js
@@ -17,7 +17,7 @@
  */
 
 import * as React from 'react';
-import { Accordion, Container, Nav, Navbar, NavDropdown, OverlayTrigger, Dropdown, Button, ButtonGroup, Offcanvas, Form } from 'react-bootstrap';
+import { Accordion, Container, Nav, Navbar, NavDropdown, Dropdown, Offcanvas } from 'react-bootstrap';
 import Image from 'next-image-export-optimizer';
 
 import Search from '../search/Search';

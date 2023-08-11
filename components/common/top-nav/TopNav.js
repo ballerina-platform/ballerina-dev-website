d90b5d16e71966cc84c488e4f88ca093a9b48a40
Sarani Mendis <saranimendisb@gmail.com>
2023-08-11 14:32:39 +0530

    Update styles

--- components/common/top-nav/TopNav.js
+++ components/common/top-nav/TopNav.js
@@ -63,7 +63,7 @@ const TopNav = (props) => {
                 <Image src={`${prefix}/images/logo/ballerina-logo-dark-grey-sl-white.svg`} height={50} width={150} alt="Ballerina Logo" />
               </Navbar.Brand>
               : <Navbar.Brand href={`${prefix}/`} className={styles.logo}>
-              <Image src={`${prefix}/images/logo/ballerina-logo-grey.svg`} height={60} width={150} alt="Ballerina Logo" />
+              <Image src={`${prefix}/images/logo/ballerina-logo-grey.svg`} height={50} width={150} alt="Ballerina Logo" />
             </Navbar.Brand>
             }
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />

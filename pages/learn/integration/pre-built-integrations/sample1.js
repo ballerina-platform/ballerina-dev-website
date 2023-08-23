fbeb76ec287dcacac8ebc5f60b27df6e16fed1f6
Sarani Mendis <saranimendisb@gmail.com>
2023-08-23 11:25:25 +0530

    Add pre-built pages

---
+++ pages/learn/integration/pre-built-integrations/sample1.js
@@ -0,0 +1,203 @@
+/**
+ * Copyright (c) 2022, WSO2 LLC (http://www.wso2.com) All Rights Reserved.
+ *
+ * WSO2 LLC licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from "react";
+import Head from "next/head";
+import { Row, Col, Container } from "react-bootstrap";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { FaRegCopy, FaCheck } from 'react-icons/fa';
+
+import Layout from "../../../../layouts/LayoutLearn";
+import { prefix } from '../../../../utils/prefix';
+import { getHighlighter } from "shiki";
+
+export async function getStaticProps() {
+  const highlighter = await getHighlighter({
+    theme: 'github-light'
+  });
+  const content = `
+service /finance on new http:Listener(8080) {
+
+  resource function post customers/[int id]/accounts(BankAccountReq req) returns BankAccount|error {
+      http:Client iban = check new("http://api.iban.com.balmock.io");
+      IbanRequest ibanReq = { country_iso: req.country ?: "US", nid: req.accountNumber };
+      IbanResponse ibanRes = check iban->/clients/api/banksuite/nid.post(ibanReq);
+      string bankCode = ibanRes.bank_code;
+      http:Client intuit = check new("http://api.intuit.com.balmock.io");
+      return check intuit->/quickbooks/v4/customers/[id]/bank\-accounts.post({ ...req, bankCode });
+  }
+}
+`;
+  var samples = { code: highlighter.codeToHtml(content.replaceAll('```', '').trim(), { lang: 'ballerina' }) };
+
+  return {
+    props: {
+      samples,
+      content
+    },
+  };
+}
+
+export default function Learn({ samples, content }) {
+
+  const [copied, setCopied] = React.useState(false);
+
+  const codeCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  }
+
+
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Ballerina is a comprehensive language that is easy to grasp for anyone with prior programming experience. Start learning with the material below."
+        />
+        <meta name="author" content="WSO2 LLC" />
+        <meta
+          name="keywords"
+          content="ballerina, learn, documentation, docs, programming language"
+        />
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <title>Sample 1</title>
+
+        {/* FB */}
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Ballerina - Learn" />
+        <meta
+          property="og:description"
+          content="Ballerina is a comprehensive language that is easy to grasp for anyone with prior programming experience. Start learning with the material below."
+        />
+        <meta
+          property="og:image"
+          itemProp="image"
+          content="https://ballerina.io/images/ballerina-generic-social-media-image-2023.png"
+        />
+
+        {/* LINKED IN */}
+        <meta property="og:title" content="Ballerina" />
+        <meta
+          property="og:image"
+          content="https://ballerina.io/images/ballerina-generic-social-media-image-2023.png"
+        />
+        <meta
+          property="og:description"
+          itemProp="image"
+          content="Ballerina is a comprehensive language that is easy to grasp for anyone with prior programming experience. Start learning with the material below."
+        />
+
+        {/* TWITTER */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@ballerinalang" />
+        <meta name="twitter:creator" content="@ballerinalang" />
+        <meta name="twitter:title" content="Ballerina" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:description"
+          content="Ballerina is a comprehensive language that is easy to grasp for anyone with prior programming experience. Start learning with the material below."
+        />
+        <meta
+          name="twitter:image"
+          content="https://ballerina.io/images/ballerina-generic-social-media-image-2023.png"
+        />
+        <meta
+          property="twitter:text:description"
+          content="Ballerina is a comprehensive language that is easy to grasp for anyone with prior programming experience. Start learning with the material below."
+        />
+        <meta
+          property="twitter:image"
+          content="https://ballerina.io/images/ballerina-generic-social-media-image-2023.png"
+        />
+      </Head>
+
+      <Layout>
+
+        <Col sm={12}>
+          <Row className="pageHeader pageContentRow llanding">
+
+            <Col xs={12}>
+              <Container>
+                <h1>Sample1</h1>
+              </Container>
+            </Col>
+
+
+          </Row>
+
+          <Row className="pageContentRow llanding">
+
+            <Col xs={12}>
+              <Container>
+                <Row>
+                  <Col xs={12} lg={6} style={{ fontSize: "18px" }}>
+                    <p>Salesforce is used by many organizations to centralize customer information,
+                      aiming to enhance customer service, streamline sales, enable collaboration and obtain sales analytics.
+                      However, managing customer interactions can be complex, involving various touch points and business units.
+                      Therefore, integrating Salesforce with all related entities and automating the exchange of customer
+                      information is essential to fully leverage its capabilities.</p>
+
+                    <p>Ballerina, being a language specifically designed for integrations, can facilitate the integration of
+                      Salesforce with all relevant systems and support any complex Salesforce integration use case.
+                    </p>
+
+                  </Col>
+                  <Col xs={12} lg={6}>
+                    <img src={`${prefix}/images/slide_diagram-new-v6-final.png`} alt="Position Ballerina" style={{ width: "-webkit-fill-available" }} />
+                  </Col>
+                </Row>
+
+              </Container>
+            </Col>
+          </Row>
+
+          <Row className="pageContentRow llanding" >
+            <Col xs={12}>
+              <Container>
+
+                <div style={{
+                  background: "#eeeeee", padding: "10px",
+                  borderRadius: "5px",
+                  marginTop: "20px",
+                  backgroundColor: "#eeeeee !important"
+                }}>
+                  <CopyToClipboard text={content}
+                    onCopy={() => codeCopy()} style={{float:"right"}}>
+                    {
+                      copied ? <FaCheck style={{ color: "20b6b0" }} title="Copied" /> : <FaRegCopy title="Copy" />
+                    }
+                  </CopyToClipboard>
+
+                  <div className="highlight" dangerouslySetInnerHTML={{ __html: samples.code }} />
+                </div>
+              </Container>
+            </Col>
+          </Row>
+
+        </Col>
+
+
+      </Layout>
+    </>
+  );
+}

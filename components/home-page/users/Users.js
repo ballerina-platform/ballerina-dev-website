43e3e0db783bc60e7dc960e0e618a4d26463afc8
praneesha <praneesha@wso2.com>
2023-12-15 14:18:44 +0530

    Update components/home-page/users/Users.js
    Co-authored-by: Thisaru Guruge <thisaru@wso2.com>

--- components/home-page/users/Users.js
+++ components/home-page/users/Users.js
@@ -59,7 +59,7 @@ export default function Users(props) {
         <Row>
           <Col sm={18} md={12} className={styles.logo}>
               <a href={`${prefix}/case-studies/ballerina-central`}><img src={`${prefix}/images/home-page/user-logos/ballerina-central.svg`} width={340} alt="Ballerina Central" title="Ballerina Central" /></a>
-              <a href={`${prefix}/case-studies/redcross-elixir`} target='_blank' rel="noreferrer"><img src={`${prefix}/images/home-page/user-logos/redcross-elixir.png`} width={280} alt="Redcross Elixir" title="Redcross Elixir" /></a>
+              <a href={`${prefix}/case-studies/redcross-elixir`} rel="noreferrer"><img src={`${prefix}/images/home-page/user-logos/redcross-elixir.png`} width={280} alt="Redcross Elixir" title="Redcross Elixir" /></a>
               <a href={`${prefix}/case-studies/raapid-ai`}><img src={`${prefix}/images/home-page/user-logos/raapid-ai.png`} width={320} alt="RAAPID" title="RAAPID" /></a>
           </Col>
         </Row>

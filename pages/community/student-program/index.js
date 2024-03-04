f9d10ace4b1ac08fe56077a5d1c7558d7f27b1e6
Sarani Mendis <saranimendisb@gmail.com>
2024-03-04 21:46:48 +0530

    Fix typo

--- pages/community/student-program/index.js
+++ pages/community/student-program/index.js
@@ -101,7 +101,7 @@ export default function StudentEngagemntProgram() {
                   <img src="/images/ballerina-mesh-grey-cropped.svg" className="background-image" alt="Background" />
                   <Col xs={12} md={12} lg={6}>
                     <h1>Student engagement program</h1>
-                    <p style={{ fontSize: "24px", fontWeight: "400", color: "#20b6b0", marginTop: "40px" }}>Ballerina is dedicated to supporting the advancement of the next generation. Our student engagement programs aim to provide university students with informative sessions that complement their academic curriculum and foster a sense of community among them.</p>
+                    <p style={{ fontSize: "24px", fontWeight: "400", color: "#20b6b0", marginTop: "40px" }}>Ballerina is dedicated to supporting the advancement of the next generation. Our student engagement program aim to provide university students with informative sessions that complement their academic curriculum and foster a sense of community among them.</p>
                   </Col>
                   <Col xs={12} md={12} lg={6} style={{ paddingTop: "35px" }}>
                     <SECarousel images={images} />
